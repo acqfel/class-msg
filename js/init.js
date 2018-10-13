24ea7aad606137283c8629fb71321a09259e0473
Felipe Acquati <felipe_acq@yahoo.com.br>
2018-10-12 22:33:29 -0300

    Added: toggle expand material icon.

--- js/init.js
+++ js/init.js
@@ -2,6 +2,22 @@
   $(function(){
 
     $('.sidenav').sidenav();
+    $('.collapsible').collapsible();
 
   }); // end of document ready
+
+  $("#expand").click(function() {
+    var icon = $("i");
+    var str = icon.text();
+
+    if ( str == 'menuexpand_more'){
+      icon.text('expand_less');
+    } else if( str == 'expand_lessexpand_less'){
+      icon.text('expand_more');
+    } else if(str == 'expand_moreexpand_more') {
+      icon.text('expand_less');
+    }
+    
+  });
+
 })(jQuery); // end of jQuery name space
